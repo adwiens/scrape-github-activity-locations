6b03854cbe3b2476ef4b9ae7e0afbf6fd6f753ff
Andrew Wiens <adwiens@gmail.com>
2019-06-27 23:11:58 -0500

    simplify index.js & drop yarn for npm

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// This script gets GHArchive JSON for a random hour from June 2018 to April 2019, then gets the
-// location of each user seen in that hour. It does this in an infinite loop and saves the JSON
-// to a file every ~15s (every 10 sequential HTTP requests).
+// This script gets GHArchive JSON for a random hour from June 2018 to June 2019, then gets the
+// location of each user seen in that hour. It does this in an infinite loop and appends the
+// JSON to a file each time it gets a new user location.
 //
-// The output file is "out.json".
-//
-// The script can be restarted, e.g. after crashing, and it will pick up where it left off; it
-// starts with a different random day, but it still picks up the data it saved before.
+// The script can be restarted, e.g. after crashing, and it will continue appending to the file.
 //
 const _ = require('lodash');
 const bb = require('bluebird');
@@ -16,82 +13,47 @@ const rp = require('request-promise-native');
 const leftPad = require('left-pad');
 const gunzip = bb.promisify(require('zlib').gunzip);
 
-const outFileName = 'out.json';
-const outFileNameBlanks = 'out-blanks.json';
-const nChunks = 128; // num concurrent http requests
-let writeCounter = 0;
-let userLocations;
-try {
-  userLocations = require(`./${outFileName}`);
-} catch (e) {
-  console.warn(`Could not open ${outFileName}. Will create`);
-  userLocations = {};
-}
-let usersWithoutLocations = [];
-try {
-  usersWithoutLocations = require(`./${outFileNameBlanks}`);
-} catch (e) {
-  console.warn(`Could not open ${outFileNameBlanks}. Will create`);
-  usersWithoutLocations = [];
-}
+const outFileName = 'user-locations.json';
+const nChunks = 64; // num concurrent http requests
 
 const randomDate = (start, end) => new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 
+/** Get string array of some randomly sampled, unique usernames from a random hour. */
 const getUsers = async () => {
-  const randDate = randomDate(new Date(2018, 5), new Date(2019, 3));
+  const randDate = randomDate(new Date(2018, 5), new Date(2019, 5));
   const year = randDate.getFullYear();
   const month = leftPad(randDate.getMonth() + 1, 2, '0');
   const day = leftPad(randDate.getDate(), 2, '0');
   const hour = randDate.getHours();
   const text = (await gunzip(await rp({ uri: `https://data.gharchive.org/${year}-${month}-${day}-${hour}.json.gz`, encoding: null }))).toString();
-  return _.uniq(text.split('\n').map(s => s.trim()).filter(s => s.length > 0).map(line => JSON.parse(line).actor.login));
+  const uniqueUsernames = _.uniq(text.split('\n').map(s => s.trim()).filter(s => s.length > 0).map(line => JSON.parse(line).actor.login));
+  return _.sampleSize(uniqueUsernames, _.round(0.05 * uniqueUsernames.length));
 };
 
 (async () => {
   while(true) {
-    console.log('Loading users json...');
+    console.log('Downloading users json...');
     try {
       const users = await getUsers();
       console.log(`Scraping ${users.length} users...`);
       console.log('Getting details for each user...');
-      await Promise.all(_.chunk(users, Math.round(users.length / nChunks)).map((chunk, parIdx) => (async () => {
+      await Promise.all(_.chunk(users, Math.round(users.length / nChunks)).map((chunk, _parIdx) => (async () => {
         for (let chunkIdx = 0; chunkIdx < chunk.length; chunkIdx++) {
           const name = chunk[chunkIdx];
-          if (!userLocations[name] && !usersWithoutLocations.find(user => user === name)) {
-            try {
-              // console.log(`Trying to get details for user ${name}...`);
-              const details = await gs(name);
-              const location = details.location.trim();
-              if (location.length > 0) {
-                userLocations[name] = location;
-                // console.log(`Got location ${location} for user ${name}`);
-              } else {
-                usersWithoutLocations.push(name);
-              }
-            } catch (e) {
-              console.warn('#################');
-              console.warn(`Could not scrape location for user ${name}; skipping.`);
-              console.warn(`Error details:`);
-              console.error(e);
-              console.warn('#################');
+          try {
+            // console.log(`Trying to get details for user ${name}...`);
+            const details = await gs(name);
+            const location = details.location.trim().split('\n')[0];
+            if (location.length > 0) {
+              await writeFile(outFileName, JSON.stringify([name, location]) + '\n', { flag: 'a' });
+              // console.log(`Saved location ${location} for user ${name}`);
             }
-            if (parIdx === 0) {
-              writeCounter++;
-              if (writeCounter % 10 === 0) {
-                // Have the first worker periodically write all the location data to the file:
-                console.log('Writing all user locations json....');
-                try {
-                  await writeFile(outFileName, JSON.stringify(userLocations, undefined, 2), {});
-                  await writeFile(outFileNameBlanks, JSON.stringify(usersWithoutLocations, undefined, 2), {});
-                } catch (e) {
-                  console.warn('#################');
-                  console.warn(`Could not save data file; skipping.`);
-                  console.warn(`Error details:`);
-                  console.error(e);
-                  console.warn('#################');
-                }
-              }
-            }
+          } catch (e) {
+            console.warn('#################');
+            console.warn(`Could not scrape location for user ${name}; skipping.`);
+            console.warn(`Error details:`);
+            console.error(e);
+            console.warn('#################');
           }
         }
       })()));
